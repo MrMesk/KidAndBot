b819716193c795e6a8341d5445e5af93fc77923e
MrMesk <mesk.utp@gmail.com>
2016-10-26 14:42:05 +0200

    Switching to ZQSD

--- Assets/Climbable.cs
+++ Assets/Climbable.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Climbable : MonoBehaviour {
+public class Climbable : MonoBehaviour 
+{
 
     public BoxCollider box;
 
-    private void Awake() {
+    private void Awake() 
+	{
         box = GetComponent<BoxCollider>();
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other) 
+	{
         Character character = other.GetComponent<Character>();
-        if(character != null) {
+        if(character != null) 
+		{
             character.Climb(this);
         }
     }
