b2e0e875b7844af130009c726b30daf26e5ffaa2
MrMesk <mesk.utp@gmail.com>
2016-11-13 17:39:13 +0100

    Destructible walls

---
+++ Assets/Scripts/DestructibleWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructibleWall : MonoBehaviour
+{
+	bool hasExploded = false;
+	public void Destruct (Vector3 direction, float bumpForce)
+	{
+		if(hasExploded == false)
+		{
+			int childCount = transform.childCount;
+			GameObject[] bricks = new GameObject[childCount];
+
+			for (int i = 0; i < childCount; ++i)
+			{
+				Vector3 bumpDir = Quaternion.Euler(Random.Range(-90f, 90f), Random.Range(-90f, 90f), Random.Range(-90f, 90f)) * direction * bumpForce;
+				Rigidbody childRigid = transform.GetChild(i).GetComponent<Rigidbody>();
+				childRigid.isKinematic = false;
+				childRigid.AddForce(bumpDir);
+			}
+
+			GetComponent<BoxCollider>().enabled = false;
+		}
+		
+
+	}
+
+}
--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class RobotController : Character 
+public class RobotController : Character
 {
 	[Space(10)]
 	[Header("Range Parameters")]
@@ -21,6 +21,7 @@ public class RobotController : Character
 	public float[] bumpForcesForward = new float[4];
 	public LayerMask bumpUpMask;
 	public LayerMask bumpForwardMask;
+	public LayerMask destructibleWallMask;
 	public Slider forceBar;
 
 	[Space(10)]
@@ -61,7 +62,7 @@ public class RobotController : Character
 
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
 		grappin = transform.Find("Grappin").GetComponent<LineRenderer>();
 
@@ -71,58 +72,58 @@ public class RobotController : Character
 		}
 		grappin.enabled = false;
 
-		if (forceBar == null) 
+		if (forceBar == null)
 		{
-			forceBar = GameObject.Find ("ForceBar").GetComponent<Slider> ();
+			forceBar = GameObject.Find("ForceBar").GetComponent<Slider>();
 		}
 	}
 
 	// Update is called once per frame
-	protected override void FixedUpdate()
+	protected override void FixedUpdate ()
 	{
-		if (!isGrabbing) 
+		if (!isGrabbing)
 		{
-			base.FixedUpdate ();
+			base.FixedUpdate();
 		}
 	}
-	protected override void Update () 
+	protected override void Update ()
 	{
 		base.Update();
 
-		ChargeManagement ();
+		ChargeManagement();
 
 		forceBar.value = Mathf.Lerp(forceBar.value, bumpForce, 0.1f);
 
-		if (Input.GetMouseButtonDown (1)) 
+		if (Input.GetMouseButtonDown(1))
 		{
-			Debug.Log ("Right Mouse Click");
-			if (!isGrabbing) 
+			Debug.Log("Right Mouse Click");
+			if (!isGrabbing)
 			{
-				AttachHook ();
-			} 
-			else 
+				AttachHook();
+			}
+			else
 			{
-				DetachHook ();
+				DetachHook();
 			}
 		}
-		if (isGrabbing) 
+		if (isGrabbing)
 		{
 			grappin.enabled = true;
-			grappin.SetPosition (0, transform.position);
-			grappin.SetPosition (1, grabbedCube.transform.position);
+			grappin.SetPosition(0, transform.position);
+			grappin.SetPosition(1, grabbedCube.transform.position);
 
-			if (Input.GetKeyDown (KeyCode.A)) 
+			if (Input.GetKeyDown(KeyCode.A))
 			{
-				Pull (grabbedNormal);
+				Pull(grabbedNormal);
 				isGrabbing = false;
 				grabbedCube = null;
 				grabbedNormal = new Vector3();
 			}
-		} 
-		else 
+		}
+		else
 		{
-			BumpUp ();
-			BumpForward ();
+			BumpUp();
+			BumpForward();
 			grappin.enabled = false;
 		}
 
@@ -133,7 +134,7 @@ public class RobotController : Character
 
 		if (Input.GetButtonDown("Jump"))
 		{
-			cameraShaker.StartCoroutine(cameraShaker.Shake(walkShakeDuration*2, walkShakeMagnitude*2));
+			cameraShaker.StartCoroutine(cameraShaker.Shake(walkShakeDuration * 2, walkShakeMagnitude * 2));
 			GameObject particleImpact = Resources.Load("Particles/ImpactJump") as GameObject;
 			particleImpact = Instantiate(particleImpact, transform.position - new Vector3(0, transform.localScale.y / 2, 0), Quaternion.identity) as GameObject;
 			particleImpact.transform.forward = Vector3.up;
@@ -142,79 +143,79 @@ public class RobotController : Character
 		}
 	}
 
-	void WalkingShake()
+	void WalkingShake ()
 	{
 		walkTimer += Time.deltaTime;
-		if(walkTimer >= walkShakeFrequency)
+		if (walkTimer >= walkShakeFrequency)
 		{
 			walkTimer = 0f;
 			cameraShaker.StartCoroutine(cameraShaker.Shake(walkShakeDuration, walkShakeMagnitude));
 			FMODUnity.RuntimeManager.PlayOneShot(step, transform.position);
 		}
 	}
-	void ChargeManagement()
+	void ChargeManagement ()
 	{
-		
-		if (Input.GetKey (KeyCode.LeftShift)) 
+
+		if (Input.GetKey(KeyCode.LeftShift))
 		{
-			Debug.Log ("Charging");
+			Debug.Log("Charging");
 			bumpForce += Time.deltaTime;
-			bumpForce = Mathf.Clamp (bumpForce, 0f, bumpTiers [3] * 1.25f);
-		} 
-		else 
+			bumpForce = Mathf.Clamp(bumpForce, 0f, bumpTiers[3] * 1.25f);
+		}
+		else
 		{
 			bumpForce -= Time.deltaTime;
-			bumpForce = Mathf.Clamp (bumpForce, 0f, bumpTiers [3] * 1.25f);
+			bumpForce = Mathf.Clamp(bumpForce, 0f, bumpTiers[3] * 1.25f);
 		}
 	}
 
-	void ForceCheck()
+	void ForceCheck ()
 	{
-		if (bumpForce >= bumpTiers [3]) 
+		if (bumpForce >= bumpTiers[3])
 		{
 			forceTier = 3;
 		}
-		else if (bumpForce >= bumpTiers [2]) 
+		else if (bumpForce >= bumpTiers[2])
 		{
 			forceTier = 2;
 		}
-		else if (bumpForce >= bumpTiers [1]) 
+		else if (bumpForce >= bumpTiers[1])
 		{
 			forceTier = 1;
-		} 
-		else 
+		}
+		else
 		{
 			forceTier = 0;
 		}
-		Debug.Log ("Force Tier : " + forceTier);
+		Debug.Log("Force Tier : " + forceTier);
 		bumpForce = 0f;
 	}
 
-	void BumpForward()
+	void BumpForward ()
 	{
-		if (Input.GetMouseButtonDown(0)) 
+		if (Input.GetMouseButtonDown(0))
 		{
-			Ray ray = new Ray (transform.position, transform.forward);
+			Ray ray = new Ray(transform.position, transform.forward);
 			RaycastHit hit;
 
-			ForceCheck ();
+			ForceCheck();
 
-			Debug.DrawRay (ray.origin, ray.direction * 20f, Color.red);
-			if (Physics.Raycast (ray, out hit, bumpForwardRange,bumpForwardMask )) 
+			Debug.DrawRay(ray.origin, ray.direction * 20f, Color.red);
+			if (Physics.Raycast(ray, out hit, bumpForwardRange, bumpForwardMask))
 			{
 				//Debug.Log ("Raycast hits !!");
 				Vector3 cubeNormal = hit.normal;
 				AnimCube animCube;
-				if (Vector3.Dot (cubeNormal, hit.transform.forward) > 0.5f ||
-					Vector3.Dot (cubeNormal, -hit.transform.forward) > 0.5f ||
-					Vector3.Dot (cubeNormal, hit.transform.right) > 0.5f ||
-					Vector3.Dot (cubeNormal, -hit.transform.right) > 0.5f) 
+				if (Vector3.Dot(cubeNormal, hit.transform.forward) > 0.5f ||
+					Vector3.Dot(cubeNormal, -hit.transform.forward) > 0.5f ||
+					Vector3.Dot(cubeNormal, hit.transform.right) > 0.5f ||
+					Vector3.Dot(cubeNormal, -hit.transform.right) > 0.5f)
 				{
-					animCube = hit.transform.GetComponent<AnimCube> ();
-					if (animCube.bumping == false) 
+					animCube = hit.transform.GetComponent<AnimCube>();
+					if (animCube.bumping == false)
 					{
-						GameObject particleImpact = Resources.Load("Particles/Impact"+forceTier) as GameObject;
-						particleImpact = Instantiate (particleImpact, hit.point, Quaternion.identity) as GameObject;
+						GameObject particleImpact = Resources.Load("Particles/Impact" + forceTier) as GameObject;
+						particleImpact = Instantiate(particleImpact, hit.point, Quaternion.identity) as GameObject;
 						particleImpact.transform.forward = cubeNormal;
 						//animCube.StartCoroutine (animCube.BumpToDir (2f, bumpForcesForward [forceTier], -cubeNormal));
 						animCube.BumpingToDir(bumpForcesForward[forceTier], -cubeNormal);
@@ -224,46 +225,68 @@ public class RobotController : Character
 				}
 				cameraShaker.StartCoroutine(cameraShaker.Shake(shakeTiersDuration[forceTier], shakeTiersMagnitude[forceTier]));
 				FMODUnity.RuntimeManager.PlayOneShot(punch, transform.position);
-			} 
+			}
+
+			else if (Physics.Raycast(ray, out hit, bumpForwardRange, destructibleWallMask))
+			{
+				//Debug.Log ("Raycast hits !!");
+				Vector3 cubeNormal = hit.normal;
+				DestructibleWall destructibleWall;
+				if (Vector3.Dot(cubeNormal, hit.transform.forward) > 0.5f ||
+					Vector3.Dot(cubeNormal, -hit.transform.forward) > 0.5f ||
+					Vector3.Dot(cubeNormal, hit.transform.right) > 0.5f ||
+					Vector3.Dot(cubeNormal, -hit.transform.right) > 0.5f)
+				{
+					destructibleWall = hit.transform.GetComponent<DestructibleWall>();
+
+					GameObject particleImpact = Resources.Load("Particles/Impact" + forceTier) as GameObject;
+					particleImpact = Instantiate(particleImpact, hit.point, Quaternion.identity) as GameObject;
+					particleImpact.transform.forward = cubeNormal;
+					destructibleWall.Destruct(-cubeNormal, bumpForcesForward[forceTier] * 20f);
+
+				}
+				cameraShaker.StartCoroutine(cameraShaker.Shake(shakeTiersDuration[forceTier], shakeTiersMagnitude[forceTier]));
+				FMODUnity.RuntimeManager.PlayOneShot(punch, transform.position);
+			}
 		}
 	}
 
-	void Pull(Vector3 dir)
+	void Pull (Vector3 dir)
 	{
-		ForceCheck ();
+		ForceCheck();
 		//grabbedCube.StartCoroutine (grabbedCube.BumpToDir (2f, bumpForcesForward [forceTier], dir));
 		grabbedCube.BumpingToDir(bumpForcesForward[forceTier], dir);
 		cameraShaker.StartCoroutine(cameraShaker.Shake(shakeTiersDuration[forceTier], shakeTiersMagnitude[forceTier]));
 		FMODUnity.RuntimeManager.PlayOneShot(pull, transform.position);
 	}
 
-	void DetachHook()
+	void DetachHook ()
 	{
 		grabbedCube = null;
 		isGrabbing = false;
 		FMODUnity.RuntimeManager.PlayOneShot(hookOff, transform.position);
 	}
 
-	void AttachHook()
+	void AttachHook ()
 	{
 		bumpForce = 0f;
 
-		Debug.Log ("Attaching hook");
-		Ray ray = new Ray (transform.position, transform.forward);
+		Debug.Log("Attaching hook");
+		Ray ray = new Ray(transform.position, transform.forward);
 		RaycastHit hit;
 
-		if (Physics.Raycast (ray, out hit, pullRange, bumpForwardMask)) 
+		if (Physics.Raycast(ray, out hit, pullRange, bumpForwardMask))
 		{
 			//Debug.Log ("Raycast hits !!");
 			Vector3 cubeNormal = hit.normal;
 			AnimCube animCube;
-			if (Vector3.Dot (cubeNormal, hit.transform.forward) > 0.5 ||
-				Vector3.Dot (cubeNormal, -hit.transform.forward) > 0.5 ||
-				Vector3.Dot (cubeNormal, hit.transform.right) > 0.5 ||
-				Vector3.Dot (cubeNormal, -hit.transform.right) > 0.5) 
+			if (Vector3.Dot(cubeNormal, hit.transform.forward) > 0.5 ||
+				Vector3.Dot(cubeNormal, -hit.transform.forward) > 0.5 ||
+				Vector3.Dot(cubeNormal, hit.transform.right) > 0.5 ||
+				Vector3.Dot(cubeNormal, -hit.transform.right) > 0.5)
 			{
-				animCube = hit.transform.GetComponent<AnimCube> ();
-				if (animCube.bumping == false) 
+				animCube = hit.transform.GetComponent<AnimCube>();
+				if (animCube.bumping == false)
 				{
 					grabbedNormal = cubeNormal;
 					grabbedCube = animCube;
@@ -272,30 +295,30 @@ public class RobotController : Character
 			}
 
 			FMODUnity.RuntimeManager.PlayOneShot(hookOn, transform.position);
-		} 
+		}
 	}
 
-	void BumpUp()
+	void BumpUp ()
 	{
-		if (Input.GetKeyDown (KeyCode.E) && mobilityState == MobilityState.GROUNDED) 
+		if (Input.GetKeyDown(KeyCode.E) && mobilityState == MobilityState.GROUNDED)
 		{
-			ForceCheck ();
+			ForceCheck();
 
 			GameObject particleImpact = Resources.Load("Particles/ImpactGround") as GameObject;
-			particleImpact = Instantiate (particleImpact, transform.position - new Vector3(0, transform.localScale.y/2,0), Quaternion.identity) as GameObject;
+			particleImpact = Instantiate(particleImpact, transform.position - new Vector3(0, transform.localScale.y / 2, 0), Quaternion.identity) as GameObject;
 			particleImpact.transform.forward = Vector3.up;
 
 			AnimCube animCube;
 
 			//Getting every block in range, but not the ones below the player and bumping them according to the bumping force
-			nearlyBlocks = Physics.OverlapSphere (transform.position, bumpRange, bumpUpMask);
-			foreach (Collider col in nearlyBlocks) 
+			nearlyBlocks = Physics.OverlapSphere(transform.position, bumpRange, bumpUpMask);
+			foreach (Collider col in nearlyBlocks)
 			{
-				Debug.Log ("Pos X Player : " + (transform.position.y - transform.localScale.y) + "Pos X Target : " + (col.transform.position.y - col.transform.localScale.y/2));
-				animCube = col.GetComponent<AnimCube> ();
-				if (col.transform.position.y - col.transform.localScale.y/2 >= transform.position.y - transform.localScale.y -1f && animCube.bumping == false) 
+				Debug.Log("Pos X Player : " + (transform.position.y - transform.localScale.y) + "Pos X Target : " + (col.transform.position.y - col.transform.localScale.y / 2));
+				animCube = col.GetComponent<AnimCube>();
+				if (col.transform.position.y - col.transform.localScale.y / 2 >= transform.position.y - transform.localScale.y - 1f && animCube.bumping == false)
 				{
-					animCube.StartCoroutine (animCube.BumpUp (2f, bumpForcesUp[forceTier]));
+					animCube.StartCoroutine(animCube.BumpUp(2f, bumpForcesUp[forceTier]));
 				}
 			}
 			cameraShaker.StartCoroutine(cameraShaker.Shake(shakeTiersDuration[forceTier], shakeTiersMagnitude[forceTier]));
