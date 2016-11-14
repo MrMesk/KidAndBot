51fa06af12312d60bedf9eb60e413c588cfce544
Charlene ROSE <charlenerose1208@gmail.com>
2016-11-14 15:49:00 +0100

    Premier Ld ( Mise en place des block movables et Unmovables ) + travail sur Particules

---
+++ Assets/Resources/Particles/Impact Bump/ActivateCrackEffect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateCrackEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.C))
+		{
+			GameObject particleImpact2 = Resources.Load("Particles/ImpactGroundBump") as GameObject;
+			//Debug.Log(particleImpact2);
+			particleImpact2 = Instantiate (particleImpact2) as GameObject;
+			//Debug.Log("Impact");
+			GameObject particleImpact = Resources.Load("Particles/CrackEffect") as GameObject;
+			//Debug.Log(particleImpact);
+			particleImpact = Instantiate (particleImpact) as GameObject;
+
+	
+
+
+		
+		}
+	}
+}
