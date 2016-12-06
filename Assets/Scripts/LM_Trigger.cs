3bbec3943b29db0c8fe86d3d5546feab3e129af2
DESKTOP-I1RU9F2\Utilisateur <charlenerose1208@gmail.com>
2016-12-06 21:14:16 +0100

    PartBot & debut Kid

---
+++ Assets/Scripts/LM_Trigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LM_Trigger : MonoBehaviour {
+
+	public GameObject toOpen;
+
+        private void OnTriggerEnter(Collider other)
+    {
+        toOpen.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        toOpen.SetActive(true);
+    }
+}
