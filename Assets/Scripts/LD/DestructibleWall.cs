25484667e46cd50c64a6f77de931b9a18a140fdd
MrMesk <mesk.utp@gmail.com>
2017-01-05 18:43:38 +0100

    Kid circuit rough is setup.

--- Assets/Scripts/LD/DestructibleWall.cs
+++ Assets/Scripts/LD/DestructibleWall.cs
@@ -17,9 +17,9 @@ public class DestructibleWall : MonoBehaviour
 				Vector3 bumpDir = Quaternion.Euler(Random.Range(-randomProjectionAngle, randomProjectionAngle), Random.Range(-randomProjectionAngle, randomProjectionAngle), Random.Range(-randomProjectionAngle, randomProjectionAngle)) * direction * bumpForce;
 				Rigidbody childRigid = transform.GetChild(i).GetComponent<Rigidbody>();
 				CrushBlocks childCrush = transform.GetChild (i).GetComponent<CrushBlocks> ();
-				childCrush.moved = true;
 				childRigid.isKinematic = false;
-				childRigid.AddForce(bumpDir);
+				childRigid.AddForce(bumpDir * 50f);
+				StartCoroutine(BlockInit(childCrush));
 			}
 
 			GetComponent<BoxCollider>().enabled = false;
@@ -27,5 +27,15 @@ public class DestructibleWall : MonoBehaviour
 		
 
 	}
+	public IEnumerator BlockInit (CrushBlocks block)
+	{
+		yield return new WaitForSeconds(0.3f);
+		if(block != null)
+		{
+			block.moved = true;
+		}
 
+	}
 }
+
+
